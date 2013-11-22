8e9cdc03e61f525a9cc89bca383320e0c2171280
nhahn <Nathan.P.Hahn@boeing.com>
2013-11-22 11:32:44 -0500

    Intial commit of the PostMortem project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostMortem::Application.config.secret_token = '5b04b7f6a344281bc45e1c73a85f7f7ebf2a3ea79a183a909c7ae96b33609de90951f16f7431a5602cc2434b7ca5b7b3786885653a7a76466f539e62befec16d'
